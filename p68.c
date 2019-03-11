1b5c84df851b4587c590c95eb81b35a7814b7b4a
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-03-11 06:24:36 +0530

    p68.c

---
+++ p68.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    int n,a[10],i,j,c,max=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c>=max)
+        {
+            max=c;
+        }
+    }
+    printf("%d",max);
+    
+}
